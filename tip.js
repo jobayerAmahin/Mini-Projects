517fb460fb5688caf879323408498263d2ad1c41
jobayerAmahin <ja.mahinmolla.2824@gmail.com>
2024-08-31 14:16:48 +0300

    Adding new js file to practice

---
+++ tip.js
@@ -0,0 +1,14 @@
+rid=["Dig","Dug","Big","Bug"];
+console.log(rid);
+
+rid.pop();
+console.log(rid);
+
+rid.push("Dust");
+console.log(rid);
+
+rid.shift();
+console.log(rid);
+
+rid.unshift("Rust");
+console.log(rid);
