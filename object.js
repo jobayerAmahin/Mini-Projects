73565dbd871c00d979f09b8c135673fc4abebcc9
jobayerAmahin <ja.mahinmolla.2824@gmail.com>
2024-09-13 22:29:19 +0300

    Adding new js file with new object related problems

---
+++ object.js
@@ -0,0 +1,9 @@
+const mahin={
+    name:'Mahin',
+    age:27,
+    act:function(num1,num2){
+        console.log(num1+num2);
+    },
+    cert:[{hsc:'Pass',ssc:'pass Highly',grade:5},{nnn:'Pass',kkk:'pass Highly',edarg:5}]
+}
+mahin.act(2,3)
