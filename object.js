870ffd4abefccaaa1c0bac6facb3c83623bf5cb4
jobayerAmahin <ja.mahinmolla.2824@gmail.com>
2024-09-13 23:49:05 +0300

    Adding new js file with new object searching related problems wrongly

--- object.js
+++ object.js
@@ -1,3 +1,4 @@
+//Task 1(Array and function inside of an object)
 const mahin={
     name:'Mahin',
     age:27,
@@ -7,3 +8,34 @@ const mahin={
     cert:[{hsc:'Pass',ssc:'pass Highly',grade:5},{nnn:'Pass',kkk:'pass Highly',edarg:5}]
 }
 mahin.act(2,3)
+
+//Task 2 (Searching specific keywords value in objects)
+
+const työpaikka=[
+    {name:'IT',paikka:'Uusima,Helsinki',palkka:2000},
+    {name:'IT',paikka:'Uusima,Espoo',palkka:2000},
+    {name:'IT',paikka:'Uusima,Vantaa',palkka:2000},
+    {name:'IT',paikka:'Uusima,Helsinki',palkka:2000},
+    {name:'IT',paikka:'Uusima,Kirkkonomi',palkka:2000},
+    {name:'IT',paikka:'Uusima,Helsinki',palkka:2000},
+    {name:'IT',paikka:'Uusima,Helsinki',palkka:2000},
+    {name:'IT',paikka:'Uusima,Vantaa',palkka:2000},
+    {name:'IT',paikka:'Uusima,Helsinki',palkka:2000},
+    {name:'IT',paikka:'Uusima,Helsinki',palkka:2000},
+    {name:'IT',paikka:'Uusima,Espoo',palkka:2000},
+    {name:'IT',paikka:'Uusima,Helsinki',palkka:2000},
+    {name:'IT',paikka:'Uusima,Kerava',palkka:2000},
+    {name:'IT',paikka:'Uusima,Helsinki',palkka:2000},
+]
+
+function etsiPaikka(objekti,paikanNimi){
+    let uusiArray=[];
+    for(let paik of työpaikka){
+        if(paik.paikka.includes(paikanNimi)){
+            uusiArray.push(paik);
+        }
+        return uusiArray
+    }
+}
+const result= etsiPaikka(työpaikka,'Helsinki');
+console.log(result);
